44db08ce414d53a544fe9497c4b8366e72f6a873
zxlkungfu <zxlkungfu@126.com>
2017-06-12 23:20:40 +0800

    first commit

---
+++ ShortVideo/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  ShortVideo
+//
+//  Created by 郑小龙 on 2017/6/12.
+//  Copyright © 2017年 alon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ ShortVideo/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  ShortVideo
+//
+//  Created by 郑小龙 on 2017/6/12.
+//  Copyright © 2017年 alon. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
